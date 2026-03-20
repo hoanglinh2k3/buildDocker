8eac0ff474026bc5bda3548078efd139f68e3dbe
hoanglinh2k3 <linhnguyenhoang544@gmail.com>
2026-03-20 15:45:30 +0700

    first commit

---
+++ src/main/java/com/example/NguyenHoangLinhApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenHoangLinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenHoangLinhApplication.class, args);
+	}
+
+}
